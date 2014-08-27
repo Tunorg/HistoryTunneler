6c9a9f1d12665b0f8f36029ff9f2f4fba1e4e5ba
Ilya Petrash <gil9red@gmail.com>
2014-08-28 00:33:08 +0600

    Добавлен модуль Story.py В этом модуле будет описана история игры, диалоги и повествование.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- History_Tunneler.py
+++ History_Tunneler.py
@@ -5,6 +5,7 @@ class Attributes:
     def __init__(self):
         pass
 
+    # Полезные и просто интересные ссылки:
     # http://ru.rpg.wikia.com/wiki/%D0%90%D1%82%D1%80%D0%B8%D0%B1%D1%83%D1%82%D1%8B
     # http://ru.rpg.wikia.com/wiki/%D0%92%D1%82%D0%BE%D1%80%D0%B8%D1%87%D0%BD%D1%8B%D0%B5_%D1%85%D0%B0%D1%80%D0%B0%D0%BA%D1%82%D0%B5%D1%80%D0%B8%D1%81%D1%82%D0%B8%D0%BA%D0%B8
     # http://ru.rpg.wikia.com/wiki/%D0%A5%D0%B0%D1%80%D0%B0%D0%BA%D1%82%D0%B5%D1%80%D0%B8%D1%81%D1%82%D0%B8%D0%BA%D0%B0
@@ -43,3 +44,8 @@ class Zombi(Character):
 z = Zombi()
 print(z)
 print(z.attr.health)
+
+
+# Проверка пользовательского модуля
+import Story
+print(Story.ENTRY)
