96dff071aba2d831782a2bf5881565dc782addff
Ilya Petrash <gil9red@gmail.com>
2014-08-29 01:20:30 +0600

    Обновление модуля Character.py
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- Character.py
+++ Character.py
@@ -1,9 +1,26 @@
 __author__ = 'ipetrash'
 
+# TODO: Переименовать в статы
 class Attributes:
     """Общий класс описывающий аттрибуты персонажей и монстров."""
     def __init__(self):
         pass
+
+    # Статы и формулы рассчета статов некоторых игр FF
+    # http://finalfantasy.wikia.com/wiki/Stats
+
+    # Характеристики в FF8
+    # http://squarefaction.ru/game/final-fantasy-viii/articles/10480-parametry-geroev-v-final-fantasy-viii
+
+    # Характеристики в FF7
+    # http://ffforever.info/index.cgi?section=ff7cc;p=experience
+    # http://www.ign.com/faqs/2004/final-fantasy-vii-stat-building-faq-452504
+
+    # Характеристики в FF6
+    # http://www.cavesofnarshe.com/ff6/stats.php
+
+    # Статы в FF9
+    # http://letao.is-a-geek.net/ff9stats/
 
     # Полезные и просто интересные ссылки:
     # http://ru.rpg.wikia.com/wiki/%D0%90%D1%82%D1%80%D0%B8%D0%B1%D1%83%D1%82%D1%8B
@@ -15,23 +32,30 @@ class Attributes:
     dx = 0  # dexterity / ловкость
     iq = 0  # intellect / интеллект
 
-    health = 0
-    max_health = 0
+    dm = 0  # damage / повреждение
+    critical = 0  # chance to deal critical / шанс нанести крит
+    hp = 0  # hit point
+    max_hp = 0
+    # health = 0
+    # max_health = 0
 
 
 class Character:
-    """Общий класс для персонажей и монстров."""
+    """Общий класс для персонажей."""
     def __init__(self):
         pass
 
     def __repr__(self):
         return "{} lvl: {}".format(self.name, self.level)
 
-    # name = CharacterName()
     name = "???"
     level = 1
 
     attr = Attributes()
+
+    def attack_to(self, other):
+        """Функция атаки персонажей."""
+        pass
 
 
 class Zombi(Character):
