8c0818920929f28ea8331fbe01b2037fca8bed5d
Ilya Petrash <gil9red@gmail.com>
2014-08-30 06:33:34 +0600

    Добавлены статы к классу Character. Сделана функция атаки. Эмитация боя.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ __author__ = 'ipetrash'
 import Story
 import Character
 
+import time
+
 if __name__ == '__main__':
     # Проверка пользовательского модуля
     print(Story.ENTRY)
@@ -10,7 +12,19 @@ if __name__ == '__main__':
     print()
     z = Character.Zombi()
     print(z)
-    s = Character.Skeleton()
-    print(s)
     g = Character.Goblin()
     print(g)
+
+    # Эмитация боя
+    # TODO: воспользоваться исключениями, чтоб отлавливать, когда кто-то умрет
+    while z.hp > 0 and g.hp > 0:
+        print("______________________")
+        z.attack_to(g)
+        g.attack_to(z)
+        time.sleep(1)
+
+    print("\n\n{} hp: {} and {} hp: {}".format(z.name, z.hp, g.name, g.hp))
+    if z.hp == 0 and g.hp == 0:
+        print("Оба сдохли!")
+    else:
+        print("{} победил!".format(z.name if z.hp else g.name))
