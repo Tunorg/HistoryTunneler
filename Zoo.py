910b7363000633c7f4c69164a8b69d19eba12984
Ilya Petrash <gil9red@gmail.com>
2014-09-03 23:30:42 +0600

    В классах-наследниках BaseCharacter добавлен вызов конструктора предка. Добавлены todo-предложения, касающиеся модуля.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- Zoo.py
+++ Zoo.py
@@ -4,10 +4,31 @@ from Char import BaseCharacter
 import Type
 
 
+# TODO: С развитием модуля Type, возможно, нет смысла в наследовании от BaseCharacter, ведь
+# все можно описать в типе. А для создания нужного существа, создавать экземпляр BaseCharacter и
+# передавать нужны тип персонажа, например для гоблина:
+# import Type
+# g = BaseCharacter()
+# g.type = Type.Goblin()
+#
+# А если передавать в конструктор, то будет выглядеть приятнее:
+# import Type
+# g = BaseCharacter(type=Type.Goblin())
+#
+# Или:
+# from Type import Goblin()
+# g = BaseCharacter(type=Goblin())
+#
+
+# TODO: переименовать модуль, название зоопарк не совсем корректно, хоть и забавно :)
+
+
 class Hero(BaseCharacter):
     """Класс Герой"""
 
     def __init__(self):
+        BaseCharacter.__init__(self)
+
         self.name = "Проходчик"
         self.description = "Собственно это я!"
         self.type = Type.Hero()
@@ -19,6 +40,8 @@ class Zombi(BaseCharacter):
     """Класс Зомби."""
 
     def __init__(self):
+        BaseCharacter.__init__(self)
+
         self.name = "Зомби"
         self.description = "Когда-то это было живым существом."
         self.type = Type.Zombi()
@@ -30,6 +53,8 @@ class Goblin(BaseCharacter):
     """Класс Гоблин."""
 
     def __init__(self):
+        BaseCharacter.__init__(self)
+
         self.name = "Гоблин"
         self.description = "Маленькое, пронырливое, трусливое зеленокожее существо."
         self.type = Type.Goblin()
