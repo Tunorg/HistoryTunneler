a618762a30109cae66ccaac4cdc8cae804d2d36c
Ilya Petrash <gil9red@gmail.com>
2014-09-07 20:44:27 +0600

    Update
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- main.py
+++ main.py
@@ -8,57 +8,63 @@ import Personage
 import time
 
 
+def sandbox(g1, g2):
+    print("Group1: {}".format(g1))
+    print("Group2: {}".format(g2))
+
+
 if __name__ == '__main__':
     # Вывод состояния персонажей и показ изменения статов при изменении уровня
     Personage.DEBUG_MODE = False  # Убираем вывод в консоль
     Personage.DEBUG_MODE_GET_SET = False  # Убираем вывод в консоль информацию о set и get методах дескрипторах
 
-    # gua_badan = Zoo.Ork()
-    # gua_badan.name = "Большой Гуа-бадан"
-    # gua_badan.desc = "Моя большая и сильная!"
-    # print(gua_badan)
-    # print("{}: '{}'".format(gua_badan.name, gua_badan.desc))
-
     # TODO: добавить бой командами с выбором действий
     #
-    #
-
     h = ClassPersonage.Hero()
-    print(h)
+    z1 = ClassPersonage.Zombi()
+    z2 = ClassPersonage.Zombi()
 
-    z = ClassPersonage.Zombi()
-    z.level = 3  # Пусть у зомби будет 3-й уровень
-    print(z)
+    g1 = [h]
+    g2 = [z1, z2]
+    sandbox(g1, g2)
 
-    Personage.DEBUG_MODE = True
-    c = 1
 
-    t = time.time()
-    while not h.dead or not z.dead:
-        print()
-        print("Раунд {}".format(c))
-        print("{}(hp: {})  VS  {} (hp: {})".format(h.name, h.hp, z.name, z.hp), end="\n ")
-
-        h.attack_to(z)
-        if z.dead:
-            break
-
-        z.attack_to(h)
-        if h.dead:
-            break
-
-        c += 1
-        time.sleep(1.5)  # Ждем 1.5 секунды
-        # input()
-
-    print("\n")
-    if h.dead:
-        print("Ты умер! Вот ты лах!")
-
-    elif z.dead:
-        print("Поздравляю чувак! Ты убил {} lvl {}!!".format(z.name, z.level))
-        h.exp += z.gives_exp
-
-    print("Бой занял {} раундов и {:.1f} секунд.".format(c, time.time() - t))
-
-    Personage.DEBUG_MODE = False
+    # h = ClassPersonage.Hero()
+    # print(h)
+    #
+    # z = ClassPersonage.Zombi()
+    # z.level = 3  # Пусть у зомби будет 3-й уровень
+    # print(z)
+    #
+    # Personage.DEBUG_MODE = True
+    # c = 1
+    #
+    # t = time.time()
+    # while not h.dead or not z.dead:
+    #     print()
+    #     print("Раунд {}".format(c))
+    #     print("{}(hp: {})  VS  {} (hp: {})".format(h.name, h.hp, z.name, z.hp), end="\n ")
+    #
+    #     h.attack_to(z)
+    #     if z.dead:
+    #         break
+    #
+    #     z.attack_to(h)
+    #     if h.dead:
+    #         break
+    #
+    #     c += 1
+    #     time.sleep(1.5)  # Ждем 1.5 секунды
+    #     # input()
+    #
+    # print("\n")
+    # if h.dead:
+    #     print("Ты умер! Вот ты лах!")
+    #
+    # elif z.dead:
+    #     print("Поздравляю чувак! Ты убил {} lvl {}!!".format(z.name, z.level))
+    #     h.exp += z.gives_exp
+    #
+    # print("Бой занял {} раундов и {:.1f} секунд.".format(c, time.time() - t))
+    #
+    # Personage.DEBUG_MODE = False
