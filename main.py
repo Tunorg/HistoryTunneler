59d9e62643d5477577488e7936fb7f093fbba943
Ilya Petrash <gil9red@gmail.com>
2014-09-15 00:05:55 +0600

    Если в группе есть больше одного персонажа одинакового класса, то нужно к их именам добавить номер. Рефакторинг.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- main.py
+++ main.py
@@ -6,44 +6,55 @@ __author__ = 'ipetrash'
 import ClassPersonage
 import BasePersonage as bp
 import time
+from collections import defaultdict
 
 
+def set_num_pers_in_group(group):
+    """
+    Функция добавляет номер к имени персонажа, если такой
+    тип персонажа уже есть в группе.
+    """
+
+    # Создаем словарь, ключем которого будет тип персонажа, а значением -- количество
+    # персонажей такого типа в группе
+    pers_count = defaultdict(int)
+
+    # Перебор персонажей в группе
+    for p in group:
+        k = type(p)  # Ключом является имя типа персонажа
+        pers_count[k] += 1  # Добавляем 1 к количеству персонажей в группе
+        c = pers_count[k]  # Получение количества персонажей такого типа в группе
+        if c > 1:  # Если персонажей больше 1
+            p.name += " {}".format(c)  # Добавление номера к имени персонажа
+
+
+# TODO: добавить бой командами с выбором действий
 def sandbox(g1, g2):
-    # TODO: если в группе есть 2 и больше персонажа одинакового класса (например, Зомби), то нужно к их
-    # именам добавить номер
-    print("Группа 1:")
-    for i, p in enumerate(g1):
-        print("{}. {}".format(i + 1, "{}(lvl {}): {}".format(p.name, p.level, type(p))))
+    """Песочница для боя между двумя группами."""
 
-    print()
-    print("Группа 2:")
-    for i, p in enumerate(g2):
-        print("{}. {}".format(i + 1, "{}(lvl {}): {}".format(p.name, p.level, type(p))))
+    # Добавление номера к персонажам в группе
+    set_num_pers_in_group(g1)
+    set_num_pers_in_group(g2)
 
-    # определим у кого во второй группе наибольшая скорость
-    # первый в списке будет иметь наибольшую скорость
-    print("\nГруппа 2, отсортированная по скорости:")
-    sorted_group2 = sorted(g2, key=lambda x: x.speed, reverse=True)
-    for p in sorted_group2:
-        print("Скорость: {}: {}".format(p.speed, "{}(lvl {}): {}".format(p.name, p.level, type(p))))
+    # Словарь, у которого ключем является персонаж, а значением номер группы,
+    # в которую входит персонаж
+    pers_number_group = dict()
+    for p in g1:
+        pers_number_group[p] = 1
+    for p in g2:
+        pers_number_group[p] = 2
 
-
+    # Объединим группы
     commons = g1 + g2
-    print()
-    print("Объединенные группы:")
-    for i, p in enumerate(commons):
-        print("{}. {}".format(i + 1, "{}(lvl {}): {}".format(p.name, p.level, type(p))))
 
-
-    # определим порядок хода:
+    # Определим порядок хода:
     # первый в списке будет иметь наибольшую скорость и, соответственно, будет первым ходить, при нескольких
     # персонажах, имеющих одинаковую скорость, положение в списке будет определяться алгоритмос функции sorted.
     sorted_commons = sorted(commons, key=lambda x: x.speed, reverse=True)
-    first = sorted_commons[0]
-    print("\n\nПервый ходит: {}".format("{}(lvl {}): {}".format(first.name, first.level, type(first))))
+
     print("\nПорядок хода:")
     for p in sorted_commons:
-        print("Скорость: {}: {}".format(p.speed, "{}(lvl {}): {}".format(p.name, p.level, type(p))))
+        print("Группа: [{}]: Скорость: {}, {} (lvl {})".format(pers_number_group[p], p.speed, p.name, p.level))
 
 
 if __name__ == '__main__':
@@ -51,53 +62,56 @@ if __name__ == '__main__':
     bp.DEBUG_MODE = False  # Убираем вывод в консоль
     bp.DEBUG_MODE_GET_SET = False  # Убираем вывод в консоль информацию о set и get методах дескрипторах
 
-
-    # TODO: добавить бой командами с выбором действий
+    # Подготовка групп к бою:
     h = ClassPersonage.Hero()
+    h.level = 3
 
     z1 = ClassPersonage.Zombi()
     z2 = ClassPersonage.Zombi()
     z3 = ClassPersonage.Goblin()
-    z4 = ClassPersonage.Ork()
+    z4 = ClassPersonage.Zombi()
+    z5 = ClassPersonage.Goblin()
+    z6 = ClassPersonage.Ork()
 
     g1 = [h]
-    g2 = [z1, z2, z3, z4]
+    g2 = [z1, z2, z3, z4, z5, z6]
+
+    # Бой:
     sandbox(g1, g2)
 
-
-    h = ClassPersonage.Hero()
-    z = ClassPersonage.Zombi()
-    z.level = 2  # Пусть у зомби будет 3-й уровень
-    print()
-    print(h)
-    print(z)
-
-    bp.DEBUG_MODE = True
-    c = 1
-
-    t = time.time()
-    while not h.dead or not z.dead:
-        print()
-        print("Раунд {}".format(c))
-        print("{}(hp: {})  VS  {} (hp: {})".format(h.name, h.hp, z.name, z.hp), end="\n ")
-
-        h.attack_to(z)
-        if z.dead:
-            break
-
-        z.attack_to(h)
-        if h.dead:
-            break
-
-        c += 1
-        time.sleep(1.5)  # Ждем 1.5 секунды
-        # input()
-
-    print("\n")
-    if h.dead:
-        print("Ты умер! Вот ты лах!")
-
-    elif z.dead:
-        print("Поздравляю чувак! Ты убил {} lvl {}!!".format(z.name, z.level))
-
-    print("Бой занял {} раундов и {:.1f} секунд.".format(c, time.time() - t))
+    # h = ClassPersonage.Hero()
+    # z = ClassPersonage.Zombi()
+    # z.level = 2  # Пусть у зомби будет 3-й уровень
+    # print()
+    # print(h)
+    # print(z)
+    #
+    # bp.DEBUG_MODE = True
+    # c = 1
+    #
+    # t = time.time()
+    # while not h.dead or not z.dead:
+    #     print()
+    #     print("Раунд {}".format(c))
+    #     print("{}(hp: {})  VS  {} (hp: {})".format(h.name, h.hp, z.name, z.hp), end="\n ")
+    #
+    #     h.attack_to(z)
+    #     if z.dead:
+    #         break
+    #
+    #     z.attack_to(h)
+    #     if h.dead:
+    #         break
+    #
+    #     c += 1
+    #     time.sleep(1.5)  # Ждем 1.5 секунды
+    #     # input()
+    #
+    # print("\n")
+    # if h.dead:
+    #     print("Ты умер! Вот ты лах!")
+    #
+    # elif z.dead:
+    #     print("Поздравляю чувак! Ты убил {} lvl {}!!".format(z.name, z.level))
+    #
+    # print("Бой занял {} раундов и {:.1f} секунд.".format(c, time.time() - t))
